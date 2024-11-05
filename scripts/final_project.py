d02f1a32ad19bebe38ff4f79d2bb0ddf8e972256
Anil Thanki <thanki.anil@gmail.com>
2024-11-05 17:09:48 +0000

    Update final_project.py - removing redundant code

--- scripts/final_project.py
+++ scripts/final_project.py
@@ -3,6 +3,7 @@ import scanpy as sc
 import anndata
 from numpy import all
 import logging
+import os
 
 adata = sc.read('input.h5')
 
@@ -13,7 +14,8 @@ qc_vars = list()
 
 
 gene_names = getattr(adata.var, gene_name)
-
+# Define the directory containing your source files
+source_dir = '.'  # Adjust to the appropriate path
 
 ad_s = sc.read('r_source.h5')
 if not all(adata.obs.index.isin(ad_s.obs.index)):
@@ -182,312 +184,32 @@ else:
 del ad_s
 
 
-ad_s = sc.read('embedding_source_0.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_0"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_0"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 0 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_1.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_1"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_1"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 1 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_2.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_2"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_2"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 2 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_3.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_3"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_3"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 3 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_4.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_4"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_4"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 4 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_5.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_5"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_5"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 5 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_6.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_6"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_6"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 6 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_7.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_7"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_7"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 7 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_8.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_8"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_8"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 8 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_9.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_9"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_9"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 9 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_10.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_10"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_10"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 10 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_11.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_11"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_11"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 11 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_12.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_12"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_12"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 12 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_13.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_13"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_13"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 13 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_14.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_14"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_14"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 14 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_15.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_15"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_15"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 15 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
-ad_s = sc.read('embedding_source_16.h5')
-if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_16"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-  keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
-  for k_to_copy in keys_to_copy:
-    suffix = ''
-    if k_to_copy in adata.obsm:
-        suffix = "_16"
-    adata.obsm[k_to_copy+suffix] = ad_s.obsm[k_to_copy]
-else:
-  logging.error("Embedding source 16 AnnData file is not compatible to be merged to main AnnData file, different cell names.")
-  sys.exit(1)
-del ad_s
+
+
+embedding_sources = [file for file in os.listdir(source_dir) if file.startswith('embedding_source_') and file.endswith('.h5')]
+
+for idx, embedding_file in enumerate(sorted(embedding_sources)):
+    ad_s = sc.read(os.path.join(source_dir, embedding_file))
+    if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
+        # Copy tsne embeddings
+        keys_to_copy = (k for k in ad_s.obsm.keys() if "tsne" in k)
+        for k_to_copy in keys_to_copy:
+          suffix = ''
+          if k_to_copy in adata.obsm:
+            suffix = f"_{idx}"
+          adata.obsm[k_to_copy + suffix] = ad_s.obsm[k_to_copy]
+        # Copy umap embeddings
+        keys_to_copy = (k for k in ad_s.obsm.keys() if "umap" in k)
+        for k_to_copy in keys_to_copy:
+          suffix = ''
+          if k_to_copy in adata.obsm
+            suffix = f"_{idx}"
+          adata.obsm[k_to_copy + suffix] = ad_s.obsm[k_to_copy]
+    else:
+        logging.error(f"Embedding source {idx} AnnData file is not compatible to be merged to main AnnData file, different cell names.")
+        sys.exit(1)
+    del ad_s
+
 
 ad_s = sc.read('uns_source_0.h5')
 if adata.n_obs == ad_s.n_obs and all(adata.obs_names == ad_s.obs_names):
