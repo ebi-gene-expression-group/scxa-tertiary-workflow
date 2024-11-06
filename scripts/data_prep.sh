2000f3002e9bbc6a842379d97ccb17637d24e349
Anil Thanki <thanki.anil@gmail.com>
2024-11-06 21:20:10 +0000

    Update data_prep.sh - fixing log and force unzip

--- scripts/data_prep.sh
+++ scripts/data_prep.sh
@@ -6,14 +6,14 @@ fi
 
 EXP_ID=$1
 
-echo "Creating ${pwd}/${EXP_ID} directory"
-mkdir ${EXP_ID}
-cd ${EXP_ID}
+echo "Creating $(pwd)/${EXP_ID} directory"
+mkdir -p $(pwd)/${EXP_ID}
+cd $(pwd)/${EXP_ID}
 
 echo "Copying data to ${pwd}/${EXP_ID}"
 cp ${SCXA_WORKFLOW_ROOT}/results/${EXP_ID}/*/bundle/${EXP_ID}.cell_metadata.tsv cell_metadata.tsv
-cp ${SCXA_WORKFLOW_ROOT}/results/${EXP_ID}/*/bundle/filtered_normalised/genes.tsv.gz . && gunzip genes.tsv.gz
-cp ${SCXA_WORKFLOW_ROOT}/results/${EXP_ID}/*/bundle/filtered_normalised/matrix.mtx.gz . && gunzip matrix.mtx.gz
-cp ${SCXA_WORKFLOW_ROOT}/results/${EXP_ID}/*/bundle/filtered_normalised/barcodes.tsv.gz . && gunzip barcodes.tsv.gz
+cp ${SCXA_WORKFLOW_ROOT}/results/${EXP_ID}/*/bundle/filtered_normalised/genes.tsv.gz . && gunzip -f genes.tsv.gz
+cp ${SCXA_WORKFLOW_ROOT}/results/${EXP_ID}/*/bundle/filtered_normalised/matrix.mtx.gz . && gunzip -f matrix.mtx.gz
+cp ${SCXA_WORKFLOW_ROOT}/results/${EXP_ID}/*/bundle/filtered_normalised/barcodes.tsv.gz . && gunzip -f barcodes.tsv.gz
 cp ${SCXA_WORKFLOW_ROOT}/results/${EXP_ID}/*/bundle/reference/gene_annotation.txt genes_metadata.tsv
 echo "Copying data for ${EXP_ID} finished"
